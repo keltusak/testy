9966d69c0fed16872cfa5b3ce733255fa91d34e7
Jan Balošák <bal41078@spseol.cz>
2024-02-14 14:07:42 +0100

    ok

--- task3/linked_list.py
+++ task3/linked_list.py
@@ -12,3 +12,22 @@ class DoublyLinkedList:
         self.tail = None
         self.length = 0
 
+    def append(self, data):
+        new_node = Node(data)
+        if self.lenght == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            new_node.prev_node = self.tail
+            self.tail = new_node
+        self.lenght += 1
+            
+    def __len__(self):
+        return self.lengt
+
+    def __getitem__(self, index):
+        node = self.head
+        for i in range(index):
+            node = node.next_node
+        return node.data    
