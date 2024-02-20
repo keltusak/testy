27fb4b8bf8d0e4ee195a57d164bf7106bcdd3fc8
keltusak <135325525+keltusak@users.noreply.github.com>
2024-02-20 15:37:11 +0100

    práce z domu, dokončení

--- task3/linked_list.py
+++ task3/linked_list.py
@@ -1,33 +1,93 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next_node = None
-        self.prev_node = None
 
+class Node:
+    def __init__(self, prev_node=None, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+        self.prev_node = prev_node
+
+    def __repr__(self):
+        return f"Node({self.data})"
 
 class DoublyLinkedList:
     def __init__(self):
-        super().__init__()
         self.head = None
         self.tail = None
         self.length = 0
 
     def append(self, data):
-        new_node = Node(data)
-        if self.lenght == 0:
-            self.head = new_node
-            self.tail = new_node
+        if self.length == 0:
+            node = Node(None, data, None)
+            self.head = node
+            self.tail = node
         else:
-            self.tail.next_node = new_node
-            new_node.prev_node = self.tail
-            self.tail = new_node
-        self.lenght += 1
-            
+            node = Node(self.tail, data, None)
+            self.tail.next_node = node
+            self.tail = node
+        self.length += 1
+
+    def insert(self, data, index):
+        index = self._translate_index(index)
+
+        if index == self.length:
+            self.append(data)
+            return
+        elif index == 0:
+            if self.length == 0:
+                self.append(data)
+            else:
+                node = Node(None, data, self.head)
+                self.head.prev_node = node
+                self.head = node
+            self.length += 1
+            return
+
+        prev_node = self._get_node(index - 1)
+        node = Node(prev_node, data, prev_node.next_node)
+        prev_node.next_node.prev_node = node
+        prev_node.next_node = node
+        self.length += 1
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current.data
+            current = current.next_node
+
     def __len__(self):
-        return self.lengt
+        return self.length
 
     def __getitem__(self, index):
-        node = self.head
-        for i in range(index):
-            node = node.next_node
-        return node.data    
+        index = self._translate_index(index)
+        return self._get_node(index).data
+
+    def __setitem__(self, index, data):
+        index = self._translate_index(index)
+        self._get_node(index).data = data
+
+    def __delitem__(self, index):
+        index = self._translate_index(index)
+        node = self._get_node(index)
+        if node.prev_node:
+            node.prev_node.next_node = node.next_node
+        else:
+            self.head = node.next_node
+        if node.next_node:
+            node.next_node.prev_node = node.prev_node
+        else:
+            self.tail = node.prev_node
+        self.length -= 1
+
+    def _translate_index(self, index):
+        if index < -self.length or index >= self.length:
+            raise IndexError("Index out of range")
+        if index < 0:
+            index += self.length
+        return index
+
+    def _get_node(self, index):
+        if index < 0 or index >= self.length:
+            raise IndexError("Index out of range")
+        current = self.head
+        for _ in range(index):
+            current = current.next_node
+        return current
